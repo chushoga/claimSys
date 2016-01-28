abb5498ecb2176c017831ad607769332ce629dd3
chushoga <chushoga@gmail.com>
2016-01-28 13:41:48 +0900

    update

--- exe/processEditFiles.php
+++ exe/processEditFiles.php
@@ -1,67 +1,69 @@
-<?php //home connect
+<?php
+	//home connect
 	require_once("../master/dbConnect.php");
 	$path = "/claimSys";
 
+	// ---------------------------------------
+	// ----- DEBUGGING for ajax to a txt file
+	// ---------------------------------------
+	function debugger(){
+		
+	// the name of the file you're writing to
+	$myFile = "data.txt";
 
-function debugger(){
-	
-// ---------------------------------------
-// ----- DEBUGGING for ajax to a txt file
-// ---------------------------------------
-// the name of the file you're writing to
-$myFile = "data.txt";
+	// opens the file for appending (file must already exist)
+	$fh = fopen($myFile, 'a');
 
-// opens the file for appending (file must already exist)
-$fh = fopen($myFile, 'a');
+	// Makes a CSV list of your post data
+	$comma_delmited_list = implode(",", $_POST) . "\n";
 
-// Makes a CSV list of your post data
-$comma_delmited_list = implode(",", $_POST) . "\n";
+	foreach($_POST as $file => $files){
+		//fwrite($fh, $file.'='.$files.', ');
+	}
 
-foreach($_POST as $file => $files){
-	//fwrite($fh, $file.'='.$files.', ');
-}
+	// Write to the file
+	fwrite($fh, $comma_delmited_list);
 
-// Write to the file
-fwrite($fh, $comma_delmited_list);
+	// You're done
+	fclose($fh);
 
-// You're done
-fclose($fh);
+	}
 
-}
-
+	// ---------------------------------------
+	// ----- SET ACTION TO $_POST['action']
+	// ---------------------------------------
 	$action = $_POST["action"];
 
+	// ---------------------------------------
+	// ----- EDIT FILES
+	// ---------------------------------------
 	if($action == "editFiles"){
-		
-		$cid = $_POST['cid'];
-		$recId = $_POST['recordBlockId']; // record block id No.
-		
-		
-		//$show = mysql_query("SELECT * FROM `records` ORDER BY `id` desc");
-		
-		//while($row = mysql_fetch_array($show)){
-			//echo "<li><b>".$row['id_dmg']."</b> : ".$row['modelNo']."</li>";
-			
-			// GET ALL IMAGES IN DIRECTORY AND DISPLAY THEM
-		
-			$fileLoc = "../recordFiles/".$cid."/".$recId."/";
-		
-			$filesImages = glob($fileLoc.'*.{JPG,jpg,png,gif}', GLOB_BRACE);
 
-			foreach($filesImages as $file) {
-			 echo "<a class='fancybox' rel='group_$recId' href='claimsys/$file' title='".substr($file, strrpos($file, '/') + 1)."'> <img src='claimsys/$file' style='width: 100px; height: 100px; float: left;'></a>";
-			}
-			
-			
-		//}
-	} else if($action == "refreshFilesBlock"){
-		
 		$cid = $_POST['cid'];
 		$recId = $_POST['recordBlockId']; // record block id No.
-		
+
+		// GET ALL IMAGES IN DIRECTORY AND DISPLAY THEM
+
 		$fileLoc = "../recordFiles/".$cid."/".$recId."/";
-			
-// GET ALL OTHER FILES (pdf,dxf etc) IN DIRECTORY AND DISPLAY THEM
+
+		$filesImages = glob($fileLoc.'*.{JPG,jpg,png,gif}', GLOB_BRACE);
+
+		foreach($filesImages as $file) {
+			echo "<a class='fancybox' rel='group_$recId' href='claimsys/$file' title='".substr($file, strrpos($file, '/') + 1)."'> <img src='claimsys/$file' style='width: 100px; height: 100px; float: left;'></a>";
+		}
+	}
+
+	// ---------------------------------------
+	// ----- REFRESH FILES BLOCK
+	// ---------------------------------------
+	else if($action == "refreshFilesBlock"){
+
+		$cid = $_POST['cid'];
+		$recId = $_POST['recordBlockId']; // record block id No.
+
+		$fileLoc = "../recordFiles/".$cid."/".$recId."/";
+
+		// GET ALL OTHER FILES (pdf,dxf etc) IN DIRECTORY AND DISPLAY THEM
 		// PFD
 			$filesPdf = glob($fileLoc.'*.{pdf,PDF}', GLOB_BRACE);
 
@@ -86,13 +88,13 @@ fclose($fh);
 			foreach($filesOther as $file) {
 				echo "<a href='$file' style='text-align: center;'><i class='fa fa-file-o' style='color: grey;'></i> ".substr($file, strrpos($file, '/') + 1)."</a><br>";
 			}
-			
-		
-	} else if($action == "addcomment"){
-		
-//		$name = $_POST['damageId_11'];
-//		$message = $_POST['modelNo_11'];
-		
+		}
+
+	// ---------------------------------------
+	// ----- ADD COMMENT
+	// ---------------------------------------
+	else if($action == "addcomment"){
+
 		$recId = $_POST['recordBlockId']; // record block id No.
 		$damageId =  $_POST['damageId_'.$recId];
 		$modelNo = $_POST['modelNo_'.$recId];
@@ -102,7 +104,7 @@ fclose($fh);
 		$records_invoiceNo = $_POST['invoiceNo_'.$recId];
 		$records_invoiceDate = $_POST['invoiceDate_'.$recId];
 		$records_invoiceGntNo = $_POST['invoiceGntNo_'.$recId];
-	
+
 		$records_currency = $_POST['currency_'.$recId];
 		$records_invoiceValue = $_POST['invoiceValue_'.$recId];
 		$records_damageType = $_POST['damageType_'.$recId];
@@ -110,59 +112,19 @@ fclose($fh);
 		$records_damageMemo_EN = $_POST['damageMemoEn_'.$recId];
 		$records_damageMemo_JP = $_POST['damageMemoJp_'.$recId];
 		$records_flgPending = $_POST['flgPending_'.$recId];
-		
+
 		$records_editedBy = $_POST['editedBy_'.$recId];
 		$cid = $_POST['cid'];
-		
+
 		$modified = date("Y-m-d");
 
-
-		
-		//$records_id_recordMaster = $_POST['id_recordMaster'.$recId];
-/*
-		$query = mysql_query("INSERT INTO `records`(
-											id_dmg,
-											modelNo,
-											tformNo,
-											orderNo,
-											spec,
-											invoiceNo,
-											invoiceDate,
-											invoiceGntNo,
-											currency,
-											invoiceValue,
-											damageType,
-											damageSize,
-											damageMemoEn,
-											damageMemoJp,
-											flagPending
-											)
-											VALUES(
-											'$damageId',
-											'$modelNo',
-											'$records_tformNo',
-											'$records_orderNo',
-											'$records_spec',
-											'$records_invoiceNo',
-											'$records_invoiceDate',
-											'$records_invoiceGntNo',
-											'$records_currency',
-											'$records_invoiceValue',
-											'$records_damageType',
-											'$records_damageSize',
-											'$records_damageMemo_EN',
-											'$records_damageMemo_JP',
-											'$records_flgPending'
-											)
-		");
-*/
 		$query = mysql_query("UPDATE `recordmaster` SET
 											`editedBy` = '$records_editedBy',
 											`modified` = '$modified'
 										WHERE
 											`id` = '$cid'
 		");
-		
+
 		$query = mysql_query("UPDATE `records` SET
 											`id_dmg` = '$damageId',
 											`modelNo` = '$modelNo',
